8db41e5e3f4a25dbebae2397d997de100363d7de
Leo Liu <109166550+dleoliu@users.noreply.github.com>
2022-10-05 04:54:39 +0000

    Converte indentation to spaces

--- answer/week1/task2.cpp
+++ answer/week1/task2.cpp
@@ -5,24 +5,24 @@ using namespace std;
 
 int main()
 {
-	int a, b, c, d;
+    int a, b, c, d;
     cout << "a b c d: ";
-	cin >> a >> b >> c >> d;
-	cout << "判定：";
+    cin >> a >> b >> c >> d;
+    cout << "判定：";
 
-	if (((a + b + c + d) % 2) == 0) // 和が偶数
-	{
-		if (( (a * b * c * d) % 2) == 0) // 積が偶数
-		{
-			cout << "和も積も偶数\n";
-		}
-		else
-		{
-			cout << "和が偶数\n";
-		}
-	}
-	else // 和が奇数の場合、積は必ず偶数になる
-	{
-		cout << "積が偶数\n";
-	}
+    if (((a + b + c + d) % 2) == 0) // 和が偶数
+    {
+        if (( (a * b * c * d) % 2) == 0) // 積が偶数
+        {
+            cout << "和も積も偶数\n";
+        }
+        else
+        {
+            cout << "和が偶数\n";
+        }
+    }
+    else // 和が奇数の場合、積は必ず偶数になる
+    {
+        cout << "積が偶数\n";
+    }
 }
